5a49a550957ef905a08aaee0b1160841141ffc36
obidano <omotetedan2@gmail.com>
2024-11-07 13:53:58 +0100

    feat: add Homepage & Boxpage

---
+++ app/src/main/java/com/orange/orangetransact/pages/BoxPage.kt
@@ -0,0 +1,56 @@
+package com.orange.orangetransact.pages
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun BoxPage(){
+
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun BoxBody(){
+    Scaffold(){
+        Column(modifier= Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Box(modifier=Modifier.background(Color.Black)
+                .height(100.dp).width(100.dp), contentAlignment = Alignment.Center){
+
+                Box(modifier=Modifier.background(Color.Yellow)
+                    .height(50.dp).width(50.dp))
+
+                Icon(Icons.Default.Call, null)
+            }
+        }
+    }
+}
+
+@Preview(device= Devices.PIXEL_6)
+@Composable
+fun BoxBodyPreview(){
+    BoxBody()
+}
---
+++ app/src/main/java/com/orange/orangetransact/pages/HomePage.kt
@@ -0,0 +1,51 @@
+package com.orange.orangetransact.pages
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun HomePage(){
+
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun HomeBody(){
+    Scaffold(containerColor = Color.White) {
+        Column(modifier= Modifier.fillMaxSize()) {
+
+            Row (modifier= Modifier.fillMaxWidth().weight(1f)) {
+
+                Box(modifier= Modifier.weight(1f)
+                    .background(Color.Green).fillMaxHeight())
+
+                Box(modifier= Modifier.weight(1f)
+                    .background(Color.Blue).fillMaxHeight())
+            }
+
+            //Text("Hello World")
+            Box(modifier= Modifier.weight(1f)
+                .background(Color.Red).fillMaxWidth())
+        }
+    }
+
+}
+
+@Composable
+@Preview
+fun HomeBodyPreview(){
+    HomeBody()
+}
