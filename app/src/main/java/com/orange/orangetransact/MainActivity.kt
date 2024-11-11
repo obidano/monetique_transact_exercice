783ef33d94140d3697ed52f31bdc5f781bd818df
obidano <omotetedan2@gmail.com>
2024-11-11 16:07:36 +0100

    feat: call dashboard page in MainActivity

--- app/src/main/java/com/orange/orangetransact/MainActivity.kt
+++ app/src/main/java/com/orange/orangetransact/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.orange.orangetransact.pages.DashboardPage
 import com.orange.orangetransact.ui.theme.OrangetransactTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OrangetransactTheme {
-
+                 DashboardPage()
             }
         }
     }
