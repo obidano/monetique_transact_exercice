46b8a31166834f667e7e76375313f79445d44a57
obidano <omotetedan2@gmail.com>
2024-11-11 16:07:03 +0100

    feat: add models ActionItemData

---
+++ app/src/main/java/com/orange/orangetransact/models/ActionItemData.kt
@@ -0,0 +1,8 @@
+package com.orange.orangetransact.models
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class ActionItemData(
+    val title: String,
+    val icon: ImageVector
+)
